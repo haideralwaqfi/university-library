b9178bcf0f72a6c78ec79541aad9fa67af73c02d
haideralwaqfi <haideralwaqfi@gmail.com>
2025-03-14 23:10:45 +0300

    finishing Home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 const Home = () => (
   <>
-    <Button>Click Me</Button>
+    <BookOverview />
+    <BookList />
   </>
-)
+);
 
-export default  Home;
+export default Home;
---
+++ components/BookList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BookList() {
+  return (
+    <section className="font-bebas-neue text-4xl text-light-100">
+      Popular Books
+    </section>
+  );
+}
+
+export default BookList;
