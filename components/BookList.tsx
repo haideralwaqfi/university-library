af6d187b472f5a0e8f203ea4767f77c20ee861ca
haideralwaqfi <haideralwaqfi@gmail.com>
2025-03-15 17:21:19 +0300

    finishing home page design

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BookCard from "./BookCard";
 
 interface Props {
   title: string;
@@ -8,8 +9,13 @@ interface Props {
 
 function BookList({ title, books, containerClassName }: Props) {
   return (
-    <section className="font-bebas-neue text-4xl text-light-100">
-      Popular Books
+    <section className={containerClassName}>
+      <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 }
